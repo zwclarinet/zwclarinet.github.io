1ca22046125c32302360780a5b99c2e3526f28db
zwclarinet <z.w.clarinet@gmail.com>
2025-03-06 23:37:58 +0000

    Deploying to gh-pages from @ zwclarinet/zwclarinet.github.io@83b81a8aeaea46342d92d431f3e12ec59bf2769d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-publications",
+          title: "publications",
+          description: "My upcoming activities.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
