4d5bd08ffe7c232261da709d128b90e391709576
zwclarinet <z.w.clarinet@gmail.com>
2025-03-06 23:22:49 +0000

    Deploying to gh-pages from @ zwclarinet/zwclarinet.github.io@4d108906263b393c09f4c78ed62270811f9a05dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Summary of my teaching experiences",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
